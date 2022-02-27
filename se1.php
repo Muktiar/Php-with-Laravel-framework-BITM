93bc6030eea0e4f355ba46c329bc3b21c576dc0f
unknown <muktiar-2>
2022-02-27 10:54:26 +0600

    first exam

--- se1.php
+++ se1.php
@@ -1,5 +1,21 @@
 <?php
-
+$f=array("m","a", "p", "pe");
+$f=array_flip($f);
+echo ($f[0])."<br>";
+$s=array("ba"=>array("po" =>"15" ,"ca"=>"da"),"cf"=>array("po"=>"17","ca"=>"ch"));
+echo $s ["ba"]["po"]."<br>";
+$s1=array("ch","gec","Bitm seip" ,"skill investment program");
+echo (array_search("Bitm seip",$s1))."<br>";
+$f1=array("apple","orange","banana");
+echo(next($f1));
+echo(next($f1))."<br>";
+$t="o'malley wins the heavyweight championship";
+echo ucwords($t)."<br>";
+echo str_pad("Salad", 5 )." is good."."<br>";
+//int $one=1;
+//echo "$one";
+//int $one=1;
+//echo "$one";
 $hello="Hello World";
 $bye="Bye";
 echo $hello;"$bye";
