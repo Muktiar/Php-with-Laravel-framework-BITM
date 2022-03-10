a363851cbfe3e60643d9d2d7d10a4b0b6b9286ca
unknown <muktiar-2>
2022-03-10 09:09:34 +0600

    get post file method work

--- Files/file.php
+++ Files/file.php
@@ -1,7 +1,10 @@
 <?php
 
 if (isset($_FILES['my_file'])){
-    echo $_FILES["my_file"]['name'];
+    echo "File name is:".$_FILES["my_file"]['name'].'<br>';
+    echo "File type is:".$_FILES["my_file"]['type'].'<br>';
+    echo "File path is:".$_FILES["my_file"]['tmp_name'].'<br>';
+    echo "File size is:".$_FILES["my_file"]['size'].'<br>';
 }
 
 echo "<pre>";
