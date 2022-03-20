447339185bcb995d9fea2d3dd4c3e5d8d2f307f3
Muktiar <muktiarh02@gmail.com>
2022-03-20 22:47:14 +0600

    folder create but not editing

---
+++ CRUD/Admin/Banners/delete.php
@@ -0,0 +1,16 @@
+<?php
+$_id = $_GET['id'];
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+$conn = new PDO("mysql:host=$servername;dbname=ecommerce302713", $username, $password);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query= "DELETE FROM `banners` where `banners`.`id`=$_id";
+$stmt=$conn->prepare($query);
+$result=$stmt->execute();
+header('Location:index.php');
+?>
