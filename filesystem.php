529c94d4ddd3c8a48673a84d2d0f735eb48badfe
1603110201249-Muktiar Hossen <55034692+Muktiar@users.noreply.github.com>
2022-03-21 13:21:05 +0600

    exam2

---
+++ filesystem.php
@@ -0,0 +1,12 @@
+<?php
+$path_parts = pathinfo('www/muktiar/Php-with-Laravel-framework-BITM/CRUD/302713/backend_seip/Dashboard/Folder/User/store.php');
+
+echo $path_parts['dirname'], "\n";
+echo "<br>";
+echo $path_parts['basename'], "\n";
+echo "<br>";
+echo $path_parts['extension'], "\n";
+echo "<br>";
+echo $path_parts['filename'], "\n";
+echo "<br>";
+?>
